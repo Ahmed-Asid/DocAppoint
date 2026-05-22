ee5780e697665af166a571c95dae9a9fbe5d22a5
Ahmed-Asid <ahmedasid249@gmail.com>
2026-05-22 23:33:46 +0600

    made the sign up page and connected better auth and mongodb for sign up functionality and data

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,17 @@
+
+import SignUp from './SignUp';
+
+export const metadata = {
+    title: 'SkillSphere - Sign Up',
+    description: 'Create an account',
+};
+
+const SignUpPage = () => {
+    return (
+        <>
+            <SignUp></SignUp>
+        </>
+    );
+};
+
+export default SignUpPage;
